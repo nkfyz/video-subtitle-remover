3e5a058ef3f6686cb4915669aebfa178570f4723
nkfyz <fyz@mail.nankai.edu.cn>
2024-09-24 11:49:16 +0800

    add deps

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
+from typing import List
 from setuptools import find_packages, setup
+
+
+def fetch_requirements(paths) -> List[str]:
+    """
+    This function reads the requirements file.
+
+    Args:
+        path (str): the path to the requirements file.
+
+    Returns:
+        The lines in the requirements file.
+    """
+    if not isinstance(paths, list):
+        paths = [paths]
+    requirements = []
+    for path in paths:
+        with open(path, "r") as fd:
+            requirements += [r.strip() for r in fd.readlines()]
+    return requirements
 
 
 setup(
@@ -10,4 +30,5 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
+    install_requires=fetch_requirements("requirements.txt"),
 )
